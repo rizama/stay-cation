1bde42988a939236852ee76ca85a4cf970d806fa
Rizky Sam Pratama <rizkysampratama@gmail.com>
2020-10-31 21:03:25 +0700

    images most picked & grid

---
+++ src/parts/MostPicked.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function MostPicked(props) {
+    return (
+        <section className="container">
+            <h4 className="mb-3">Most Picked</h4>
+            <div className="row">
+                <div className="col-4"></div>
+                <div className="col-8"></div>
+            </div>
+        </section>
+    )
+}
+
+export default MostPicked
