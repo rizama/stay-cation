acd699cb55d4bb8012797f88cfb4e90746cee6b4
Rizky Sam Pratama <rizkysampratama@gmail.com>
2020-10-31 21:58:23 +0700

    styling most picked #1

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -3,12 +3,14 @@ import Header from 'parts/Header'
 
 import landingPage from 'json/landingPage.json';
 import Hero from 'parts/Hero';
+import MostPicked from 'parts/MostPicked';
 
 export default function LandingPage(props) {
     return (
         <>
             <Header {...props}></Header>
             <Hero data={landingPage.hero}></Hero>
+            <MostPicked data={landingPage.mostPicked}/>
         </>
     )
 }
--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -1,12 +1,36 @@
+import Button from 'elements/Button'
 import React from 'react'
 
 function MostPicked(props) {
     return (
         <section className="container">
             <h4 className="mb-3">Most Picked</h4>
-            <div className="row">
-                <div className="col-4"></div>
-                <div className="col-8"></div>
+            <div className="container-grid">
+                {
+                    props.data.map((item, index) => {
+                        return (
+                            <div className={`item column-4 ${index === 0 ? "row-2" : "row-1"}`} key={item._id}>
+                                <div className="card card-featured">
+                                    <div className="tag">
+                                        ${item.price}
+                                        <span className="font-weight-light">per {item.unit}</span>
+                                    </div>
+                                    <figure className="img-wrapper">
+                                        <img src={item.imageUrl} alt={item.name} className="img-cover" />
+                                    </figure>
+                                    <div className="meta-wrapper">
+                                        <Button className="streched-link d-block text-white" type="link" href={`/properties/${item._id}`}>
+                                            <h5>{item.name}</h5>
+                                        </Button>
+                                        <span>
+                                            {item.city}, {item.country}
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
             </div>
         </section>
     )
