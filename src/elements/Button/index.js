dfc94d70bde294098770a75ae00c2a52cb913c3a
Rizky Sam Pratama <rizkysampratama@gmail.com>
2020-10-31 12:53:15 +0700

    persiapan komponen button

---
+++ src/elements/Button/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import propTypes from 'prop-types'
+
+
+export default function Button(props) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+Button.propTypes = {
+    type: propTypes.oneOf(["button", "link"]),
+    onClick: propTypes.func,
+    target: propTypes.string,
+    className: propTypes.string,
+    isDisabled: propTypes.bool,
+    isLoading: propTypes.bool,
+    isSmall: propTypes.bool,
+    isLarge: propTypes.bool,
+    isPrimary: propTypes.bool,
+    isBlock: propTypes.bool,
+    isExternal: propTypes.bool,
+    hasShadow: propTypes.bool,
+}
