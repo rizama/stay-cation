e586f095b3d096b7997c4de36c53513c9ae849cd
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-03-14 10:09:33 +0700

    ^update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import 'assets/scss/styles.scss';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import LandingPage from 'pages/LandingPage';
-import Exampe from 'pages/Example'
-
+import ExampleInputDate from 'pages/ExampleInputDate';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={LandingPage}></Route>
+        <Route path="/" component={ExampleInputDate}></Route>
       </Router>
     </div>
   );
