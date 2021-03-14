a833d2f6076cd2f318c105b6bf26205db69366a0
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-03-14 13:52:34 +0700

    ^implement breadcrumb

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import 'assets/scss/styles.scss';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import LandingPage from 'pages/LandingPage';
-import ExampleInputDate from 'pages/ExampleInputDate';
+// import LandingPage from 'pages/LandingPage';
+// import ExampleInputDate from 'pages/ExampleInputDate';
+import ExampleBreadcrumb from 'pages/ExampleBreadcrumb';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={ExampleInputDate}></Route>
+        <Route path="/" component={ExampleBreadcrumb}></Route>
       </Router>
     </div>
   );
