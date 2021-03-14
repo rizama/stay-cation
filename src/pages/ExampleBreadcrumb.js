c35a4bd20681e7ebbb915b88f6d83c058396c215
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-03-14 13:52:56 +0700

    + example breadcrumb

---
+++ src/pages/ExampleBreadcrumb.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import Breadcrumb from "elements/Breadcrumb";
+
+
+export default class ExampleBreadcrumb extends Component {
+    render() {
+        const breadcrumbList = [
+            { pageTitle: "Home", pageHref: "" },
+            { pageTitle: "House Details", pageHref: "" }
+        ];
+
+        return (
+            <div className="container">
+                <div
+                    className="row align-items-center justify-content-center"
+                    style={{ height: "100vh" }}
+                >
+                    <div className="col-auto">
+                        <Breadcrumb data={breadcrumbList}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
