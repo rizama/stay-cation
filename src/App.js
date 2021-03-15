b9a8912162a7388bf0b5d0563feca624752ae8d5
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-03-15 21:23:53 +0700

    + page detail title part 1

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import 'assets/scss/styles.scss';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import LandingPage from 'pages/LandingPage';
+import LandingPage from 'pages/LandingPage';
 // import ExampleInputDate from 'pages/ExampleInputDate';
-import ExampleBreadcrumb from 'pages/ExampleBreadcrumb';
+// import ExampleBreadcrumb from 'pages/ExampleBreadcrumb';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={ExampleBreadcrumb}></Route>
+        <Route path="/" component={LandingPage}></Route>
       </Router>
     </div>
   );
