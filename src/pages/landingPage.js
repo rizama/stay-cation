38103130e8dafe7661361f21ba52374f268a5864
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-03-15 22:08:52 +0700

    ^ add useEffect

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -1,5 +1,5 @@
-import React, { useRef } from 'react'
-import Header from 'parts/Header'
+import React, { useRef, useEffect } from 'react';
+import Header from 'parts/Header';
 
 import landingPage from 'json/landingPage.json';
 import Hero from 'parts/Hero';
@@ -9,16 +9,21 @@ import Testimony from 'parts/Testimony';
 import Footer from 'parts/Footer';
 
 export default function LandingPage(props) {
-    const ref = useRef(null)
+    const ref = useRef(null);
+
+    useEffect(() => {
+        window.title = "Staycation | Home";
+        window.scrollTo(0, 0);
+    }, []);
 
     return (
         <>
             <Header {...props}></Header>
             <Hero refMostPicked={ref} data={landingPage.hero}></Hero>
             <MostPicked refMostPicked={ref} data={landingPage.mostPicked} />
-            <Categories data={landingPage.categories}/>
-            <Testimony data={landingPage.testimonial}/>
-            <Footer/>
+            <Categories data={landingPage.categories} />
+            <Testimony data={landingPage.testimonial} />
+            <Footer />
         </>
-    )
+    );
 }
