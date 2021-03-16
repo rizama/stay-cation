1a36fe9e2524f97e4e5c488ed41c3620c274eaba
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-03-16 22:00:13 +0700

    + page detail description

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -5,11 +5,12 @@ import PageDetailTitle from "parts/PageDetailTitle";
 import FeaturedImage from 'parts/FeaturedImage';
 
 import ItemDetails from "json/itemDetails.json";
+import PageDetailDescription from 'parts/PageDetailDescription';
 
 export default class DetailPage extends Component {
 
     componentDidMount() {
-        window.title = "Detail Page";
+        document.title = "Detail Page";
         window.scrollTo(0, 0);
     }
 
@@ -27,7 +28,15 @@ export default class DetailPage extends Component {
                     breadcrumb={breadcrumb}
                     data={ItemDetails}
                 />
-                <FeaturedImage data={ItemDetails.imageUrls}/>
+                <FeaturedImage data={ItemDetails.imageUrls} />
+                <section className="container">
+                    <div className="row">
+                        <div className="col-7 pr-5">
+                            <PageDetailDescription data={ItemDetails}></PageDetailDescription>
+                        </div>
+                        <div className="col-5">Booking Form</div>
+                    </div>
+                </section>
             </>
         );
     }
