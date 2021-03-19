bc7e2b020e302715184b13c3310fb0353ba2eea1
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-03-19 22:11:20 +0700

    ^implement checkout page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'assets/scss/styles.scss';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import PageLanding from 'pages/PageLanding';
 import DetailPage from 'pages/DetailPage';
+import Checkout from 'pages/Checkout';
 // import ExampleInputDate from 'pages/ExampleInputDate';
 // import ExampleBreadcrumb from 'pages/ExampleBreadcrumb';
 
@@ -13,6 +14,7 @@ function App() {
       <Router>
         <Route exact path="/" component={PageLanding}></Route>
         <Route path="/properties/:id" component={DetailPage}></Route>
+        <Route path="/checkout" component={Checkout}></Route>
       </Router>
     </div>
   );
