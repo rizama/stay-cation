7e2b96acd1005cb9ce73747abd51e950be444781
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-03-20 13:43:50 +0700

    +component numbering with style

---
+++ src/elements/Stepper/Numbering/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+import propTypes from 'prop-types';
+
+import './index.scss';
+
+function Numbering({ style, className, data, current }) {
+
+    const keysOfData = Object.keys(data);
+
+    return (
+        <Fade>
+            <ol className={["stepper", className].join(" ")} style={ }>
+                {keysOfData.map((list, index) => {
+                    let isActive = list === current ? 'active' : '';
+                    if (index + 1 === keysOfData.length) {
+                        isActive = "";
+                        return null;
+                    }
+
+                    return (
+                        <li key={`list-${index}`} className={[isActive].join(" ")}>
+                            {index + 1}
+                        </li>
+                    );
+                })}
+            </ol>
+        </Fade>
+    );
+}
+
+Numbering.propTypes = {
+    className: propTypes.string,
+    data: propTypes.object,
+    current: propTypes.string
+};
+
+export default Numbering;
+
